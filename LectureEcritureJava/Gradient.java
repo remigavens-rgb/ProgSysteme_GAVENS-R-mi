072bc79fe36c0bb5f8f76a576c922963b4df48de
REMI GAVENS <remi.gavens@iut.local>
2026-09-15 18:25:31 +0200

    TPFini

--- LectureEcritureJava/Gradient.java
+++ LectureEcritureJava/Gradient.java
@@ -1,17 +1,21 @@
 public class Gradient {
     public static void main(String[] args) {
-        Image img = new Image(200, 100);
+        int hauteur = 100;
+        int largeur = 200;
+    
+        Image img = new Image(largeur, hauteur);
 
         // Génération du dégradé de bleu
-        for (int y = 0; y < hauteur; y++) {
-            for (int x = 0; x < largeur; x++) {
-                int bleu = 0; // quel calcule ?
+        for (int y = 0; y < img.getHeight(); y++) {
+            for (int x = 0; x < img.getWidth(); x++) {
+                int bleu = x + 0; 
                 img.setPixel(x, y, 0, 0, bleu);
             }
         }
 
         try {
-            img.save("gradient.ppm");
+            img.save_txt("Gradient.ppm");
+			img.save_bin("GradientByte.ppm");
             System.out.println("Dégradé créé avec succès !");
         } catch (Exception e) {
             System.err.println("Erreur lors de la création du dégradé : " + e.getMessage());

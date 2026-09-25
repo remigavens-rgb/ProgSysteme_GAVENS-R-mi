abfef114022401cd611cc18fb8e2e5cf7cb204f9
REMI GAVENS <remi.gavens@iut.local>
2026-09-25 15:36:14 +0200

    TP2 vendredi soir

---
+++ SystemeFichiersVirtuel/MemoryManager.java
@@ -0,0 +1,76 @@
+import java.io.*;
+
+public class MemoryManager {
+
+    public static final int BLOCK_SIZE = 512;
+    public static final int TOTAL_MEMORY = 1024 * 1024;
+    public static final int NUM_BLOCKS =
+            TOTAL_MEMORY / BLOCK_SIZE;
+
+    public static final int SUPERBLOCK_OFFSET = 0;
+    public static final int BITMAP_OFFSET = BLOCK_SIZE;
+    public static final int INODE_TABLE_OFFSET =
+            2 * BLOCK_SIZE;
+    public static final int DATA_OFFSET =
+            129 * BLOCK_SIZE;
+
+    public static final int INODE_SIZE = 128;
+
+    public static final int INODE_TABLE_SIZE =
+            DATA_OFFSET - INODE_TABLE_OFFSET;
+
+    public static final int MAX_INODES =
+            INODE_TABLE_SIZE / INODE_SIZE;
+
+    private byte[] memory;
+
+    public MemoryManager() {
+        this.memory = new byte[TOTAL_MEMORY];
+        initializeFilesystem();
+    }
+
+    private void initializeFilesystem() {
+        writeSuperblock();
+
+        // TODO:
+        // Réserver les blocs système 0 à 128.
+		for (int indice = 0; indice < 16; indice++ ) {
+			memory[BITMAP_OFFSET + indice] = (byte)0xFF;
+		}
+    }
+
+    private void writeSuperblock() {
+        // TODO:
+        // Utiliser Utils pour écrire les métadonnées.
+
+        Utils.writeString(
+                memory,
+                SUPERBLOCK_OFFSET,
+                "MYFS1.0",
+                16);
+
+        Utils.writeInt(
+                memory,
+                SUPERBLOCK_OFFSET + 16,
+                BLOCK_SIZE);
+
+        Utils.writeInt(
+                memory,
+                SUPERBLOCK_OFFSET + 20,
+                TOTAL_MEMORY);
+
+        Utils.writeInt(
+                memory,
+                SUPERBLOCK_OFFSET + 24,
+                NUM_BLOCKS);
+
+        Utils.writeInt(
+                memory,
+                SUPERBLOCK_OFFSET + 28,
+                MAX_INODES);
+    }
+
+    public byte[] getFilesystemMemory() {
+        return memory; 
+    }
+}
